35f46fd72c7c3c2493f0ec5ba888b2f3d597deb7
Kyikon <kyikon@163.com>
2023-08-05 15:59:40 +0800

    viewmodel

---
+++ FIT5032_MyViewModel/FIT5032_MyViewModel/Models/SampleFormViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyViewModel.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+
+    //新建一个类
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+
+        public string FirstName { get; set;}
+
+        public string LastName { get; set;}
+
+    }
+}
